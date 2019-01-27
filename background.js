8caa0719e52c2752d8ac17eab8c73ff8698fea3a
unknown <boxboy12345@hotmail.com>
2019-01-26 23:20:20 -0500

    Added replaceTrigger with takes a string and a max length and checks that string for triggers and preplaces them with there appriprite symbol Also added TESTprintSymbols which prints the gloabal veriable symbols for testing Also added setSymbols which is currently justsets symbol to to default values, in future will read from a .json

--- background.js
+++ background.js
@@ -31,33 +31,94 @@ function deactivateIME() {
 function setSymbol()
 {
 	
-	symbols = [{symbol:'=',triggers:["eql","equal","equals"]}];
+	symbols = [{symbol:"∀",triggers:["for all"]},{symbol:'=',triggers:["eql","equal","equals"]}];
 	
 };
 
 function TESTprintSymbol()
 {
 	
-	console.log("hello");
+	console.log("Printing Symbols:");
 	console.log(symbols);
 };
 
-function replaceTrigger()
+function replaceTrigger(string,maxLength)
 {
-	localSymbols = symbols;
+	var localSymbols = symbols.slice(0);
+	var stringToReturn = ""
 	
-	for (i = 0; i < 1; i++) 
+	if (string[string.length-1] == "`")
+	{string = string.slice(0,string.length-1);}
+	
+	console.log("The string is:" + string)
+	
+	for (i = string.length - 1; i >= Math.max(string.length - maxLength,0); i--) 
 	{
-     localSymbols.forEach(function (stp) 
+		
+		/*if (string[i] == " ") //if the current char is a space it just skipps it and goes back to the top
 		{
+			continue;
+		}*/
+		
+		var revIndex = string.length - (i+1); //revIndex is the current distance from the end of the srtring, 0 = end of string
+		var shortString = string.slice(string.length - (revIndex+1), string.length);
+		console.log("revIndex is now:" + revIndex);
+		console.log("Short string is now:" + shortString);
+		var symbolsToRemove = [];
+		localSymbols.forEach(function (stp) 
+		{
+			var triggersToRemove = [];
 			stp.triggers.forEach(function (trigger)
 			{
-				console.log(trigger);
+				if (trigger.length < revIndex)
+				{
+					triggersToRemove.push(trigger)
+					console.log("Added " +  trigger + " to triggersToRemove, because it is too short")
+				}
+				
+				if (trigger[trigger.length - (revIndex+1)] != string[i])
+				{
+					triggersToRemove.push(trigger);
+					console.log("Added " +  trigger + " to triggersToRemove, because it dosent match");
+				}
+				
+				if (trigger == shortString)
+				{
+					console.log("Found Match! it is: " + trigger);
+					stringToReturn = string.slice(0,i) + stp.symbol;	
+				}
+				//console.log(trigger);
 			});
+			
+			triggersToRemove.forEach(function (symbol) { stp.triggers.splice(stp.triggers.indexOf(symbol),1) });
+			
+			if (stp.triggers.length == 0) 
+			{ 
+				symbolsToRemove.push(stp); 
+				console.log("Added " +  stp.symbol + " to symbolsToRemove")
+			}});
+		
+		symbolsToRemove.forEach(function (stp) 
+		{ 
+			localSymbols.splice(localSymbols.indexOf(stp),1)
+			console.log("Removed " + stp.symbol + " localSymbols")
 		});
+		
 	}
 	
+	if (stringToReturn == "")
+	{
+		return string + "`"; //if no match is found
+	}
+	else
+	{
+		return stringToReturn;
+	}
+	
+	
 }
+
+
 
 /*	
 		localSymbols.forEach(function (stp) 
