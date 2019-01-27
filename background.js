a78ef68e36167a7f14ef94743d0558e214c0c325
Ben Potter <37202707+iamaduck@users.noreply.github.com>
2019-01-26 23:20:41 -0500

    Fixed activating and deactivating
    With great effort. It now reloads the extension in order to allow the user to type in password fields.

--- background.js
+++ background.js
@@ -1,13 +1,14 @@
 var context_id = -1;
 
+var active = false;
+
 console.log("Shalin - Background script started");
 
-chrome.input.ime.onFocus.addListener(function(context) {
-	console.log("Hello"); //'onFocus:' + context.contextID
+chrome.input.ime.onFocus.addListener(function focusWindow(context) {
 	context_id = context.contextID;
 });
 
-chrome.input.ime.onKeyEvent.addListener(function(engineID, keyData) {
+chrome.input.ime.onKeyEvent.addListener(function backTick(engineID, keyData) {
 		console.log("Hello");
 		if (keyData.type == "keydown" && keyData.key.match(/`/)) {
 			chrome.input.ime.commitText({"contextID": context_id, "text": "hello"});
@@ -17,24 +18,26 @@ chrome.input.ime.onKeyEvent.addListener(function(engineID, keyData) {
 		}
 });
 
-function activateIME(sender) {
-	console.log("Shalin - Chrome IME activated by " + sender.tab.url);
+function activateIME() {
+	console.log("Shalin - Chrome IME activated");
 	chrome.input.ime.activate();
 }
 
-function deactivateIME(sender) {
-	console.log("Shalin - Chrome IME deactivated by " + sender.tab.url);
-	chrome.input.ime.deactivate();
+function deactivateIME() {
+	console.log("Shalin - Chrome IME deactivated");
+	chrome.runtime.reload();							// I know this is gross, but it's the best way to do this where Chrome will allow typing in password/protected text fields.
 }
 
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
-		if (request.toggleState == "enable") {
-			activateIME(sender);
-			sendResponse({state: "Shalin - Chrome IME enabled"});
-		} else if (request.toggleState == "disable") {
-			deactivateIME(sender);
-			sendResponse({state: "Shalin - Chrome IME disabled"});
+		console.log("Shalin - Message received");
+		console.log(request.toggle)
+		if (request.toggle == true) {
+			activateIME();
+			sendResponse({state: true});
+		} else if (request.toggle == false) {
+			deactivateIME();
+			sendResponse({state: false});
 		}
 	}
 );
--- popup/popup.js
+++ popup/popup.js
@@ -1,9 +1,13 @@
 function toggleIME(toggleState) {
 	console.log("Shalin - Toggling IME...");
 	chrome.runtime.sendMessage({toggle: toggleState}, function(response) {
-		console.log(response.state);
+		if (response.state == true) {
+			console.log("Shalin - Chrome IME activated");
+		} else if (response.state == false) {
+			console.log("Shalin - Chrome IME deactivated");
+		}
 	});
 };
 
-document.getElementById('enableIME').onclick = toggleIME("enable");
-document.getElementById('disableIME').onclick = toggleIME("disable");
+document.getElementById("enableIME").onclick = function(){toggleIME(true)};
+document.getElementById("disableIME").onclick = function(){toggleIME(false)};
